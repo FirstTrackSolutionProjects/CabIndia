ea639d937ac423726ee43621d8444c608f3f17d8
adyaananyamishra <adyaananyamishra@email.com>
2025-08-21 16:42:31 +0530

    chore: updated footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -65,7 +65,7 @@ return (
         {/* Copyright Section */}
         
         <div className="mt-6 text-l bg-black text-center">
-          Copyright &copy; 2025, Developed by First Track Solution technologies
+          Copyright &copy; 2025, Developed by First Track Solution Technologies
         </div>
       </footer>
     </>
